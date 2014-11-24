b0c89686ee325be4b841c1d7c5506b9888e6a5b7
DerNapster <christopher.werning@gmail.com>
2014-11-24 17:05:03 +0100

    update

--- config/application.rb
+++ config/application.rb
@@ -19,6 +19,7 @@ module LeezeMs
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+    config.autoload_paths += %W(#{config.root}/lib)
 
     config.generators do |g|
       g.fixture_replacement :factory_girl
